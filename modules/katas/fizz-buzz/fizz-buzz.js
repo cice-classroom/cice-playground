0769fb50a41649507407b06e1ab071b94f4bbd56
César Alberca <cesalberca@gmail.com>
2020-02-05 17:53:48 +0100

    Solve first implementation

--- modules/katas/fizz-buzz/fizz-buzz.js
+++ modules/katas/fizz-buzz/fizz-buzz.js
@@ -1,3 +1,14 @@
-export function fizzBuzz() {
-  return [1, 2, 'fizz']
+export function fizzBuzz(number) {
+  const results = []
+  for (let i = 1; i < number + 1; i++) {
+    if (i % 3 === 0) {
+      results.push('fizz')
+    } else if (i % 5 === 0) {
+      results.push('buzz')
+    } else {
+      results.push(i)
+    }
+  }
+
+  return results
 }
--- modules/katas/fizz-buzz/fizz-buzz.spec.js
+++ modules/katas/fizz-buzz/fizz-buzz.spec.js
@@ -2,8 +2,20 @@ import { fizzBuzz } from './fizz-buzz'
 
 describe('fizzBuzz', () => {
   it('should replace a number divisible by 3 by fizz', () => {
-    const actual = fizzBuzz(4)
+    const actual = fizzBuzz(3)
 
     expect(actual).toEqual([1, 2, 'fizz'])
+  })
+
+  it('should replace a number divisible by 5 by buzz', () => {
+    const actual = fizzBuzz(5)
+
+    expect(actual).toEqual([1, 2, 'fizz', 4, 'buzz'])
+  })
+
+  it('should replace every number divisible by 3 by fizz', () => {
+    const actual = fizzBuzz(6)
+
+    expect(actual).toEqual([1, 2, 'fizz', 4, 'buzz', 'fizz'])
   })
 })
