a05a679b4077d70e856d6267ee82b9342b1a08db
César Alberca <cesalberca@gmail.com>
2020-02-06 18:34:19 +0100

    Implement second test

--- modules/katas/calculator/calculator.js
+++ modules/katas/calculator/calculator.js
@@ -1,5 +1,15 @@
+// @ts-check
+
 export class Calculator {
+  /**
+   * @param {string} string
+   * @returns {number}
+   */
   calculate(string) {
-    return Number(string[0]) + Number(string[4])
+    const numbers = string
+      .replace(/ /g, '')
+      .split('+')
+      .map(Number)
+    return numbers.reduce((sum, current) => sum + current)
   }
 }
--- modules/katas/calculator/calculator.spec.js
+++ modules/katas/calculator/calculator.spec.js
@@ -9,4 +9,13 @@ describe('Calculator', () => {
 
     expect(actual).toBe(2)
   })
+
+  it('should calculate a sum with more than 2 numbers', () => {
+    const given = '1 + 1 + 1'
+    const calculator = new Calculator()
+
+    const actual = calculator.calculate(given)
+
+    expect(actual).toBe(3)
+  })
 })
