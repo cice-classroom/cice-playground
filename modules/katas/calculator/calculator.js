a3d41586e2d7a129503f4ac4e2e93ce45ef35637
César Alberca <cesalberca@gmail.com>
2020-02-06 18:40:39 +0100

    Implement fourth test

--- modules/katas/calculator/calculator.js
+++ modules/katas/calculator/calculator.js
@@ -6,10 +6,12 @@ export class Calculator {
    * @returns {number}
    */
   calculate(string) {
+    const isSum = string.includes('+')
+    const operationSeparator = isSum ? '+' : '-'
     const numbers = string
       .replace(/ /g, '')
-      .split('+')
+      .split(operationSeparator)
       .map(Number)
-    return numbers.reduce((sum, current) => sum + current)
+    return numbers.reduce((sum, current) => (isSum ? sum + current : sum - current))
   }
 }
--- modules/katas/calculator/calculator.spec.js
+++ modules/katas/calculator/calculator.spec.js
@@ -27,4 +27,13 @@ describe('Calculator', () => {
 
     expect(actual).toBe(35)
   })
+
+  it('should handle substractions', () => {
+    const given = '1 - 1'
+    const calculator = new Calculator()
+
+    const actual = calculator.calculate(given)
+
+    expect(actual).toBe(0)
+  })
 })
