f146f91bec053f99e78a00600d4f7eaf5e447f0f
César Alberca <cesalberca@gmail.com>
2020-02-06 14:56:47 +0100

    Implement first test

--- modules/katas/change-calculator/change-calculator.spec.js
+++ modules/katas/change-calculator/change-calculator.spec.js
@@ -1,11 +1,11 @@
 import { ChangeCalculator } from './change-calculator'
 
 describe('bank', () => {
-  it('should give me back change', () => {
+  it('should return the minimum exact change', () => {
     const given = 50
 
     const actual = new ChangeCalculator().calculate(given)
 
-    expect(actual).toEqual([20, 20, 10])
+    expect(actual).toEqual([50])
   })
 })
