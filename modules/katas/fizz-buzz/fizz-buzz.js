6c1b96938b4fd2399ccfc944a6a5f10c6549e612
César Alberca <cesalberca@gmail.com>
2020-02-07 17:30:29 +0100

    Add missing tests

--- modules/katas/fizz-buzz/fizz-buzz.js
+++ modules/katas/fizz-buzz/fizz-buzz.js
@@ -1,3 +1,5 @@
 export function fizzBuzz(number) {
-  return Array.from({ length: number }, (_, k) => k + 1).map(x => (x % 3 === 0 ? 'fizz' : x % 5 === 0 ? 'buzz' : x))
+  return Array.from({ length: number }, (_, k) => k + 1).map(x =>
+    x % 3 === 0 ? 'fizz' : x % 5 === 0 ? 'buzz' : x
+  )
 }
