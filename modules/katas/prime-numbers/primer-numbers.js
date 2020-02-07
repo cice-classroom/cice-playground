f2db2982576de532e78cb05beb8df47c16cdec76
César Alberca <cesalberca@gmail.com>
2020-02-07 17:35:07 +0100

    Implement second test case

--- modules/katas/prime-numbers/primer-numbers.js
+++ modules/katas/prime-numbers/primer-numbers.js
@@ -4,6 +4,20 @@ export class PrimeNumbersFactorer {
    * @returns {number[]}
    */
   factor(number) {
-    return [2, 5]
+    const primes = []
+    for (let i = 2; i < number; i++) {
+      if (this.#isPrimeNumber(i) && number % i === 0) {
+        primes.push(i)
+      }
+    }
+    return primes
+  }
+
+  /**
+   * @param number
+   * @returns {boolean}
+   */
+  #isPrimeNumber(number) {
+    return true
   }
 }
--- modules/katas/prime-numbers/primer-numbers.spec.js
+++ modules/katas/prime-numbers/primer-numbers.spec.js
@@ -1,7 +1,16 @@
 import { PrimeNumbersFactorer } from './primer-numbers'
 
 describe('DecomposePrimeNumbers', () => {
-  it('should factor a number into prime numbers that multiplied by each other will give back the initial value', () => {
+  it('should factor a number into prime numbers', () => {
+    const given = 6
+    const primeNumbersFactorer = new PrimeNumbersFactorer()
+
+    const actual = primeNumbersFactorer.factor(given)
+
+    expect(actual).toEqual([2, 3])
+  })
+
+  it('should factor a number into prime numbers with non sequential values', () => {
     const given = 10
     const primeNumbersFactorer = new PrimeNumbersFactorer()
 
