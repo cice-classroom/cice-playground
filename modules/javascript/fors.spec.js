da4cf1493285524ed5d0cd3b4bf7839c02c83d8b
César Alberca <cesalberca@gmail.com>
2020-02-16 14:03:07 +0100

    Add for tests

---
+++ modules/javascript/fors.spec.js
@@ -0,0 +1,19 @@
+describe('fors', () => {
+  it('should iterate using for in', () => {})
+
+  it('should iterate using for in with not own properties', () => {})
+
+  it('should iterate using for in with own properties', () => {})
+
+  it('should iterate over array with for of', () => {})
+
+  it('should iterate over a string with for of', () => {})
+
+  it('should iterate over a map with for of', () => {})
+
+  it('should iterate over a generator function', () => {})
+
+  it('should iterate over an iterable object', () => {})
+
+  it('should iterate asynchronously', async () => {})
+})
