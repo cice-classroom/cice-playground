95463a01afae8a2c8608874a787011ad94cdc268
César Alberca <cesalberca@gmail.com>
2020-02-16 13:13:11 +0100

    Create map tests

---
+++ modules/javascript/maps.spec.js
@@ -0,0 +1,17 @@
+describe('map', () => {
+  it('should set a value with key foo and value bar', () => {})
+
+  it('should set some value and get it', () => {})
+
+  it('should tell me the size', () => {})
+
+  it('should check that it has some value', () => {})
+
+  it('should check that it has some value after deleting a value', () => {})
+
+  it('should delete all values', () => {})
+
+  it('should execute a callback for each value', () => {})
+
+  it('should be transformed to an array', () => {})
+})
