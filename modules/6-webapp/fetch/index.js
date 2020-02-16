accadf4c149142a91fe4e6e11b8a6601461a09ab
César Alberca <cesalberca@gmail.com>
2020-02-16 23:03:45 +0100

    Improve styling

--- modules/6-webapp/fetch/index.js
+++ modules/6-webapp/fetch/index.js
@@ -3,7 +3,7 @@ import { Carousel } from './carousel'
 
 async function getComicUrl(id) {
   const response = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://xkcd.com/${id + 1}/info.0.json`,
+    `https://cors-anywhere.herokuapp.com/https://xkcd.com/${id + 600}/info.0.json`,
     {
       method: 'GET'
     }
