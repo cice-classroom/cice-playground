f0337d3c2d608a8677fe1494aff84d09b64ecf01
César Alberca <cesalberca@gmail.com>
2020-03-11 11:42:47 +0100

    Create examples project

---
+++ modules/10-reactjs/examples/src/custom-hooks.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+export function useCounter(initialValue = 0) {
+  const [counter, setCounter] = useState(initialValue)
+
+  const incrementCounter = () => setCounter(counter + 1)
+
+  return {
+    counter,
+    incrementCounter
+  }
+}
+
+export function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export function Header() {
+  const { counter, incrementCounter } = useCounter(10)
+  return <button onClick={() => incrementCounter()}>Click me {counter}</button>
+}
+
+export function Footer() {
+  const { counter, incrementCounter } = useCounter()
+  return <button onClick={() => incrementCounter()}>Click me {counter}</button>
+}
