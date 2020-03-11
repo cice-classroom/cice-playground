68e2f1669148a427e2e0a87a1715d53272034247
César Alberca <cesalberca@gmail.com>
2020-03-11 19:19:27 +0100

    Implement on click

--- modules/10-reactjs/yes-no/src/app.tsx
+++ modules/10-reactjs/yes-no/src/app.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styles from './app.module.css'
-import { bind } from "./bind";
+import { bind } from './bind'
 
 // Data Transfer Object
 interface YesNoDto {
@@ -13,6 +13,7 @@ const cx = bind(styles)
 
 export function App() {
   const [image, setImage] = useState('')
+  const [counter, setCounter] = useState(0)
 
   const fetchImage = async () => {
     const response = await fetch('https://yesno.wtf/api')
@@ -22,11 +23,11 @@ export function App() {
 
   useEffect(() => {
     fetchImage()
-  }, [])
+  }, [counter])
 
   return (
     <div className="App">
-      <img className={cx('image')} src={image} />
+      <img className={cx('image')} src={image} onClick={() => setCounter(counter + 1)} />
     </div>
   )
 }
