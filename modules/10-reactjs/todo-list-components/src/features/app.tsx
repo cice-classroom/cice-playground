e683c640b3cb81bae8eaafce75443d1b6dd0919e
César Alberca <cesalberca@gmail.com>
2020-03-12 21:09:24 +0100

    Create todo list components

---
+++ modules/10-reactjs/todo-list-components/src/features/app.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { TodoList } from './todo-list/todo-list'
+import { Todo } from './todo'
+import { TodoCreate } from './todo-create/todo-create'
+
+export function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  async function createTodo(todoText: string) {
+    const newTodo: Todo = { id: Math.random() * 1000, text: todoText, completed: false }
+    setTodos([...todos, newTodo])
+  }
+
+  function completeTodo(id: number) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+
+        return todo
+      })
+    )
+  }
+
+  return (
+    <main>
+      <TodoList todos={todos} onCompleteTodo={completeTodo}></TodoList>
+      <TodoCreate onCreate={createTodo} todos={todos} />
+    </main>
+  )
+}
---
+++ modules/10-reactjs/todo-list-components/src/features/todo-create/todo-create.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { Button } from '../../core/components/button/button'
+import { Todo } from '../todo'
+
+interface Props {
+  onCreate(todoText: string): void
+  todos: Todo[]
+}
+
+export const TodoCreate: React.FunctionComponent<Props> = ({ onCreate, todos }) => {
+  const [todoText, setTodoText] = useState('')
+  const isTodoDuplicated = todos.map(todo => todo.text).includes(todoText)
+
+  const clearTodo = () => setTodoText('')
+
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault()
+        onCreate(todoText)
+        clearTodo()
+      }}
+    >
+      <label>
+        Todo
+        <input value={todoText} onChange={event => setTodoText(event.target.value)} />
+      </label>
+      <Button onClick={clearTodo}>Clear todo</Button>
+      <Button theme={'primary'} submit isDisabled={isTodoDuplicated}>
+        Create todo
+      </Button>
+    </form>
+  )
+}
