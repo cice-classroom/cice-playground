45bd0da41aa9f95a2014b2230e1d229b9a95a090
César Alberca <cesalberca@gmail.com>
2020-03-16 19:24:05 +0100

    Paint coordinates

--- modules/katas/maps/react-maps/src/app.tsx
+++ modules/katas/maps/react-maps/src/app.tsx
@@ -1,26 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import styles from './app.module.css'
 import { bind } from './bind'
-import { Map, TileLayer } from 'react-leaflet'
 
 const cx = bind(styles)
 
 export function App() {
-  const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
+  const [coordinates, setCoordinates] = useState({ longitude: 0, latitude: 0 })
+
   useEffect(() => {
-    navigator.geolocation.watchPosition(({ coords }) => {
-      const { latitude, longitude } = coords
-      setPosition({ latitude, longitude })
+    const id = navigator.geolocation.watchPosition(({ coords }) => {
+      const { longitude, latitude } = coords
+      setCoordinates({ longitude, latitude })
     })
-  })
+
+    return () => navigator.geolocation.clearWatch(id)
+  }, [])
+
   return (
     <div className={cx('app')}>
-      <Map center={[position.latitude, position.longitude]} zoom={13}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </Map>
+      {coordinates.longitude} - {coordinates.latitude}
     </div>
   )
 }
