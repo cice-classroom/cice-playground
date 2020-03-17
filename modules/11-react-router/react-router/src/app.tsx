a67035a2e82c6158d7d3145f44bda9a25732a5e8
César Alberca <cesalberca@gmail.com>
2020-03-17 21:35:25 +0100

    Add home route

--- modules/11-react-router/react-router/src/app.tsx
+++ modules/11-react-router/react-router/src/app.tsx
@@ -1,24 +1,15 @@
 import React from 'react'
-import logo from './logo.svg'
 import './app.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-export function App() {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <h1>Home</h1>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
