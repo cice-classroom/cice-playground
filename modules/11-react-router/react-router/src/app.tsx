a6aabb0383622b9ad20746461a48ce57959198d1
César Alberca <cesalberca@gmail.com>
2020-03-17 21:44:47 +0100

    Add users

--- modules/11-react-router/react-router/src/app.tsx
+++ modules/11-react-router/react-router/src/app.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import './app.css'
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route, Switch, useParams } from 'react-router-dom'
 import { Header } from './header'
+
+export const UserDetail: React.FC = () => {
+  const { name } = useParams()
+  return (
+    <>
+      <h1>User {name}</h1>
+    </>
+  )
+}
 
 export const App: React.FC = () => {
   return (
@@ -11,13 +20,17 @@ export const App: React.FC = () => {
         <Route path="/" exact>
           <h1>Home</h1>
         </Route>
-        <Route path="/users">
+
+        <Route path="/users" exact>
           <h1>Users</h1>
           {['pepito', 'menganito', 'fulanito'].map(name => (
-            <Link to={`users/${name}`} key={name}>
+            <Link to={`/users/${name}`} key={name}>
               {name}
             </Link>
           ))}
+        </Route>
+        <Route path="/users/:name">
+          <UserDetail />
         </Route>
       </Switch>
     </Router>
