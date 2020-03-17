d3774af07062a1e24655a2fba5a5e79009a237ae
César Alberca <cesalberca@gmail.com>
2020-03-17 21:55:52 +0100

    Add nested route

--- modules/11-react-router/react-router/src/app.tsx
+++ modules/11-react-router/react-router/src/app.tsx
@@ -1,13 +1,42 @@
 import React from 'react'
 import './app.css'
-import { BrowserRouter as Router, Link, Route, Switch, useParams } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  useParams,
+  useHistory,
+  useRouteMatch
+} from 'react-router-dom'
 import { Header } from './header'
+
+export const UserDetailTasks: React.FC = () => {
+  return (
+    <ul>
+      {['cuarentena', 'dar un paseo por la cocina', 'salir a comprar'].map(task => (
+        <li key={task}>{task}</li>
+      ))}
+    </ul>
+  )
+}
 
 export const UserDetail: React.FC = () => {
   const { name } = useParams()
+  const history = useHistory()
+  const { url, path } = useRouteMatch()
   return (
     <>
       <h1>User {name}</h1>
+      <button onClick={() => history.push(`${url}/tasks`)}>Tasks</button>
+      <Switch>
+        <Route path={path} exact>
+          <h2>User</h2>
+        </Route>
+        <Route to={`${path}/tasks`} exact>
+          <UserDetailTasks />
+        </Route>
+      </Switch>
     </>
   )
 }
