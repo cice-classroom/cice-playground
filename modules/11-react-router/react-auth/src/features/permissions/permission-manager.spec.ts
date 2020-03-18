648466530b53ae41b42016d0be354d91e8ac2025
César Alberca <cesalberca@gmail.com>
2020-03-18 21:48:13 +0100

    Refactor

--- modules/11-react-router/react-auth/src/features/permissions/permission-manager.spec.ts
+++ modules/11-react-router/react-auth/src/features/permissions/permission-manager.spec.ts
@@ -3,51 +3,46 @@ import { PermissionManager } from './permission-manager'
 
 describe('PermissionManager', () => {
   it('should check if a single permission is matched', () => {
-    const given = [Permission.CAN_EDIT_USER]
     const permissionManager = new PermissionManager()
     permissionManager.permissions = [Permission.CAN_EDIT_USER]
 
-    const actual = permissionManager.can(given)
+    const actual = permissionManager.can([Permission.CAN_EDIT_USER])
 
     expect(actual).toBe(true)
   })
 
   it('should check if one of several permissions are matched', () => {
-    const given: Permission[] = [Permission.CAN_EDIT_USER]
     const permissionManager = new PermissionManager()
     permissionManager.permissions = [Permission.CAN_EDIT_USER, Permission.CAN_SUBSCRIBE]
 
-    const actual = permissionManager.can(given)
+    const actual = permissionManager.can([Permission.CAN_EDIT_USER])
 
     expect(actual).toBe(true)
   })
 
   it('should check if all of several permissions are matched', () => {
-    const given: Permission[] = [Permission.CAN_EDIT_USER, Permission.CAN_SUBSCRIBE]
     const permissionManager = new PermissionManager()
     permissionManager.permissions = [Permission.CAN_EDIT_USER, Permission.CAN_SUBSCRIBE]
 
-    const actual = permissionManager.can(given)
+    const actual = permissionManager.can([Permission.CAN_SUBSCRIBE, Permission.CAN_EDIT_USER])
 
     expect(actual).toBe(true)
   })
 
   it('should check if one of the permissions is not matched', () => {
-    const given: Permission[] = [Permission.CAN_SUBSCRIBE]
     const permissionManager = new PermissionManager()
     permissionManager.permissions = [Permission.CAN_EDIT_USER]
 
-    const actual = permissionManager.can(given)
+    const actual = permissionManager.can([Permission.CAN_SUBSCRIBE])
 
     expect(actual).toBe(false)
   })
 
   it('should check if one of the permissions of several permissions is not matched', () => {
-    const given: Permission[] = [Permission.CAN_SUBSCRIBE]
     const permissionManager = new PermissionManager()
     permissionManager.permissions = [Permission.CAN_EDIT_USER]
 
-    const actual = permissionManager.can(given)
+    const actual = permissionManager.can([Permission.CAN_SUBSCRIBE])
 
     expect(actual).toBe(false)
   })
--- modules/11-react-router/react-auth/src/features/permissions/permission-manager.ts
+++ modules/11-react-router/react-auth/src/features/permissions/permission-manager.ts
@@ -3,7 +3,7 @@ import { Permission } from './permission'
 export class PermissionManager {
   permissions: Permission[] = []
 
-  can(permissions: Permission[]): boolean {
-    return permissions.every(x => this.permissions.includes(x))
+  can(requiredPermissions: Permission[]): boolean {
+    return requiredPermissions.every(x => this.permissions.includes(x))
   }
 }
