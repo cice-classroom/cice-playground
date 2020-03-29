f5996adfbf3dda7a8222772479965c0f16664684
César Alberca <cesalberca@gmail.com>
2020-03-29 18:28:44 +0200

    Use own http

--- modules/10-reactjs/yes-no/src/infrastructure/http-client.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const httpClient = axios.create({
-  baseURL: 'https://yesno.wtf/api'
-})
-
-export { httpClient }
--- modules/10-reactjs/yes-no/src/infrastructure/yes-no-repository-factory.ts
+++ modules/10-reactjs/yes-no/src/infrastructure/yes-no-repository-factory.ts
@@ -1,9 +1,10 @@
 import { YesNoRepository } from '../domain/yes-no-repository'
 import { YesNoHttpRepository } from './yes-no-http-repository'
 import { YesNoDtoToYesNoMapper } from './yes-no-dto-to-yes-no-mapper'
+import { Http } from './http'
 
 export class YesNoRepositoryFactory {
   static get(): YesNoRepository {
-    return new YesNoHttpRepository(new YesNoDtoToYesNoMapper())
+    return new YesNoHttpRepository(new YesNoDtoToYesNoMapper(), new Http(window.fetch.bind(window)))
   }
 }
