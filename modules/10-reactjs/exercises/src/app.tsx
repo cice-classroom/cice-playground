af6a24c9dc6a1c74c6804e76ad4f768fbac69889
César Alberca <cesalberca@gmail.com>
2020-03-31 21:00:21 +0200

    Implement third exercise

--- modules/10-reactjs/exercises/src/app.tsx
+++ modules/10-reactjs/exercises/src/app.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import { ExerciseOne } from './exercise-one'
 import { ExerciseTwo } from './exercise-two'
+import { ExerciseThree } from './exercise-three'
 
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Link to="/exercise-one">Exercise one</Link>
       <Link to="/exercise-two">Exercise two</Link>
+      <Link to="/exercise-three">Exercise three</Link>
 
       <Route path="/exercise-one">
         <ExerciseOne />
       </Route>
       <Route path="/exercise-two">
         <ExerciseTwo />
+      </Route>
+      <Route path="/exercise-three">
+        <ExerciseThree />
       </Route>
     </BrowserRouter>
   )
---
+++ modules/10-reactjs/exercises/src/exercise-three.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export const ExerciseThree: React.FC = () => {
+  const [value, setValue] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const characters = event.target.value
+
+    setValue(
+      characters
+        .split('')
+        .filter(character => !['a', 'e', 'i', 'o', 'u'].includes(character))
+        .join('')
+    )
+  }
+
+  return (
+    <div>
+      {value}
+      <label>
+        Input
+        <input onChange={handleChange} value={value} />
+      </label>
+    </div>
+  )
+}
