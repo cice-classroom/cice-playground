895db4f796038e699e6eb2bd821ab014a3bf0f20
César Alberca <cesalberca@gmail.com>
2020-03-31 19:45:30 +0200

    Solve first exercise

--- modules/10-reactjs/exercises/src/exercise-one.tsx
+++ modules/10-reactjs/exercises/src/exercise-one.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
+export const ExerciseOneFor: React.FC = () => {
+  const lis: JSX.Element[] = []
+
+  for (let i = 1; i <= 50; i++) {
+    lis.push(<li>{i}</li>)
+  }
+
+  return <ul>{lis}</ul>
+}
+
 export const ExerciseOne: React.FC = () => {
-  return <h1>Exercise one</h1>
+  const range = Array.from({ length: 50 }, (key, value) => value)
+
+  return (
+    <ul>
+      {range.map(x => (
+        <li>{x + 1}</li>
+      ))}
+    </ul>
+  )
 }
