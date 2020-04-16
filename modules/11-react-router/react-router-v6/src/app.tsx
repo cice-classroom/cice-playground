c75df79b624b60640c194a8544aa002836015b0b
César Alberca <cesalberca@gmail.com>
2020-04-16 21:24:10 +0200

    Remove react router dom types

--- modules/11-react-router/react-router-v6/src/app.tsx
+++ modules/11-react-router/react-router-v6/src/app.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from 'react'
 import './app.css'
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 import { Header } from './header'
 
 const UserDetail = lazy(() => import('./user-detail-page'))
@@ -10,24 +10,24 @@ export const App: React.FC = () => {
     <Router>
       <Header />
       <Suspense fallback={<h1>Loading</h1>}>
-        <Switch>
-          <Route path="/" exact>
+        <Routes>
+          <Route path="/">
             <h1>Home</h1>
           </Route>
 
-          <Route path="/users" exact>
+          <Route path="/users">
             <h1>Users</h1>
             {['pepito', 'menganito', 'fulanito'].map(name => (
-              <Link to={`/users/${name}`} key={name}>
+              <Link to={name} key={name}>
                 {name}
               </Link>
             ))}
           </Route>
-          <Route path="/users/:name" component={UserDetail} />
+          <Route path=":name" element={UserDetail} />
           <Route>
             <h1>404</h1>
           </Route>
-        </Switch>
+        </Routes>
       </Suspense>
     </Router>
   )
