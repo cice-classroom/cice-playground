fe9a6e8f274962344a80089252631d0fdd19480d
César Alberca <cesalberca@gmail.com>
2020-04-21 20:05:43 +0200

    Add dev tools

--- modules/12-redux/redux-simple/index.ts
+++ modules/12-redux/redux-simple/index.ts
@@ -2,7 +2,10 @@ import { createStore } from 'redux'
 import { reducer } from './reducer'
 import { increment } from './action-creators'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })
+)
 
 document.querySelector('#button')?.addEventListener('click', () => {
   store.dispatch(increment())
