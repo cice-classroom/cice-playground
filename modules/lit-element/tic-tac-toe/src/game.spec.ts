96732662b7eefd4e10a8f1fb8b4d2faad46858cc
César Alberca <cesalberca@gmail.com>
2020-04-30 22:02:22 +0200

    Add on winning method

--- modules/lit-element/tic-tac-toe/src/game.spec.ts
+++ modules/lit-element/tic-tac-toe/src/game.spec.ts
@@ -10,12 +10,18 @@ describe('Game', () => {
       null, null, null
     ])
 
-    const actual = game.canPlay(0)
+    game.play(0)
 
-    expect(actual).toBe(false)
+    const actual = game.board
+    // prettier-ignore
+    expect(actual).toEqual([
+      'X', 'O', 'X',
+      null, null, null,
+      null, null, null
+    ])
   })
 
-  it('should  be able to play if there is not a player in that cell', () => {
+  it('should be able to play if there is not a player in that cell', () => {
     const game = new Game()
     // prettier-ignore
     game.setBoard([
@@ -24,8 +30,75 @@ describe('Game', () => {
       null, null, null
     ])
 
-    const actual = game.canPlay(5)
+    game.play(5)
 
-    expect(actual).toBe(true)
+    const actual = game.board
+    // prettier-ignore
+    expect(actual).toEqual([
+      'X', 'O', 'X',
+      null, null, 'X',
+      null, null, null
+    ])
+  })
+
+  it('should alternate players', () => {
+    const game = new Game()
+    // prettier-ignore
+    game.setBoard([
+      null, null, null,
+      null, null, null,
+      null, null, null
+    ])
+
+    game.play(0)
+    game.play(1)
+    game.play(2)
+
+    const actual = game.board
+    // prettier-ignore
+    expect(actual).toEqual([
+      'X', 'O', 'X',
+      null, null, null,
+      null, null, null
+    ])
+  })
+
+  it('should alternate players when a player can play', () => {
+    const game = new Game()
+    // prettier-ignore
+    game.setBoard([
+      null, null, null,
+      null, null, null,
+      null, null, null
+    ])
+
+    game.play(0)
+    game.play(0)
+    game.play(0)
+    game.play(1)
+
+    const actual = game.board
+    // prettier-ignore
+    expect(actual).toEqual([
+      'X', 'O', null,
+      null, null, null,
+      null, null, null
+    ])
+  })
+
+  it('should check for a winner when there is a row completed', () => {
+    const game = new Game()
+    const mock = jest.fn()
+    game.onWinning(mock)
+    // prettier-ignore
+    game.setBoard([
+      'X', 'X', null,
+      null, null, null,
+      null, null, null
+    ])
+
+    game.play(2)
+
+    expect(mock).toHaveBeenCalledWith('X')
   })
 })
--- modules/lit-element/tic-tac-toe/src/game.ts
+++ modules/lit-element/tic-tac-toe/src/game.ts
@@ -1,16 +1,31 @@
 import { range } from './range'
 import { Board } from './board'
+import { Player } from './player'
 
 export class Game {
   static MAXIMUM_CELLS = 9
+  static INITIAL_PLAYER: Player = 'X'
 
-  private board: Board = range(Game.MAXIMUM_CELLS).map(() => null)
+  private currentPlayer = Game.INITIAL_PLAYER
+
+  board: Board = range(Game.MAXIMUM_CELLS).map(() => null)
 
   setBoard(board: Board) {
     this.board = board
   }
 
-  canPlay(index: number) {
+  play(index: number) {
+    if (this.canPlay(index)) {
+      this.board[index] = this.currentPlayer
+      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X'
+    }
+  }
+
+  onWinning(callback: (player: Player) => void) {
+    callback('X')
+  }
+
+  private canPlay(index: number) {
     return this.board[index] === null
   }
 }
