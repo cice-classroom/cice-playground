2aa84c93fe69a2bfdb74fbd54078973dfcacfc14
César Alberca <cesalberca@gmail.com>
2020-04-30 21:17:54 +0200

    Add basic board

--- modules/lit-element/tic-tac-toe/src/tic-tac-toe.ts
+++ modules/lit-element/tic-tac-toe/src/tic-tac-toe.ts
@@ -1,9 +1,48 @@
-import { customElement, LitElement } from 'lit-element'
+import { css, customElement, LitElement } from 'lit-element'
 import { html } from 'lit-html'
+import { general } from './general'
 
 @customElement('tic-tac-toe')
 export class TicTacToe extends LitElement {
+  static get styles() {
+    return [
+      general,
+      css`
+        .game {
+          min-height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .board {
+          display: grid;
+          max-width: 600px;
+          grid-template-columns: repeat(3, 200px);
+          grid-template-rows: repeat(3, 200px);
+          grid-gap: 20px;
+        }
+
+        .cell {
+          background-color: rebeccapurple;
+        }
+      `
+    ]
+  }
+
+  private onCellClicked(_index: number) {}
+
+  private range(number: number) {
+    return Array.from({ length: number }, (_key, value) => value)
+  }
+
   render() {
-    return html`<h1><app-button @click="${() => alert('hi')}">Start game</app-button></h1>`
+    return html`<main class="game">
+      <section class="board">
+        ${this.range(9).map(
+          cell => html`<div @click="${() => this.onCellClicked(cell)}" class="cell"></div>`
+        )}
+      </section>
+    </main>`
   }
 }
