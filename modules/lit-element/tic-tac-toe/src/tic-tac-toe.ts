650e1f0d876da917a6ed3aea9999e25406957f4b
César Alberca <cesalberca@gmail.com>
2020-04-30 20:22:03 +0200

    Rename project

--- modules/lit-element/tic-tac-toe/src/tic-tac-toe.ts
+++ modules/lit-element/tic-tac-toe/src/tic-tac-toe.ts
@@ -1,8 +1,8 @@
 import { customElement, LitElement } from 'lit-element'
 import { html } from 'lit-html'
 
-@customElement('tic-tac-joe')
-export class TicTacJoe extends LitElement {
+@customElement('tic-tac-toe')
+export class TicTacToe extends LitElement {
   render() {
     return html`<h1>Hello world</h1>`
   }
