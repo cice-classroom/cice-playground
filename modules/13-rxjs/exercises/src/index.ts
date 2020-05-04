7523c9a1546083a55a79e25d970b7ec3f73acd78
César Alberca <cesalberca@gmail.com>
2020-05-04 21:27:57 +0200

    Add index code

--- modules/13-rxjs/exercises/src/index.ts
+++ modules/13-rxjs/exercises/src/index.ts
@@ -1,9 +1,11 @@
 import { double } from './exercise-1'
 import { tap } from 'rxjs/operators'
 
-double([1, 2, 3, 4, 5]).pipe(
-  tap(x => {
-    const output = document.querySelector('#exercise-1')!
-    output.innerHTML += `Value ${x} emitted<br/>`
-  })
-).subscribe()
+double([1, 2, 3, 4, 5])
+  .pipe(
+    tap(x => {
+      const output = document.querySelector('#exercise-1')!
+      output.innerHTML += `Value ${x} emitted<br/>`
+    })
+  )
+  .subscribe()
