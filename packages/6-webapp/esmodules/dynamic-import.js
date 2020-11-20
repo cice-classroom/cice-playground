fb39b5175efd81bcaefe2a9fe770ab14e441fd89
César Alberca <cesalberca@gmail.com>
2020-11-20 19:48:10 +0100

    Use var

--- packages/6-webapp/esmodules/dynamic-import.js
+++ packages/6-webapp/esmodules/dynamic-import.js
@@ -1,7 +1,7 @@
-const value = 'bar'
+var value = 'bar'
 // https://github.com/tc39/proposal-top-level-await
 // https://developer.mozilla.org/en-US/docs/Glossary/IIFE
-;(async () => {
+;(async function () {
   const module = await import(`./${value}.js`)
   console.warn(module.sayHello())
 })()
