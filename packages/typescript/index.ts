8ccc481366912c236230d02c7db8139cc597b696
César Alberca <cesalberca@gmail.com>
2020-12-12 09:16:16 +0100

    Add more TypeScript examples

--- packages/typescript/index.ts
+++ packages/typescript/index.ts
@@ -1,14 +1,20 @@
-const foo: string = 'bar'
+const foo: string = 'foo'
 const bar: number = 1
 const baz: boolean = true
 const qux: string[] = ['bar', 'baz', 'qux']
+
+// Type inference
+const fooInferred = 'foo'
+const barInferred = 1
+const bazInferred = true
+const quxInferred = ['bar', 'baz', 'qux']
 
 // Null
 const n: null = null
 
 const a: string = 'bar'
 // KO: a = null
-const b: string | null = 'bar'
+const b: string | null = Math.random() > 0.5 ? null : 'bar'
 // OK: b = null
 
 if (b !== null) {
@@ -23,8 +29,12 @@ function sum(a: number, b: number = 1): number {
 const result = sum(2).toFixed(4)
 console.log(result)
 
+const substract = (a: number, b: number = 1): number => a + b
+const result2 = substract(3)
+console.log(result2)
+
 function concatenate(a: string, b: string | null): string {
-  if (b != null) {
+  if (b !== null) {
     return a.concat(b)
   }
 
@@ -42,11 +52,25 @@ interface Person {
   name: string
   age: number
   job?: string
+  pet: string | undefined
+}
+
+// Types
+type Car = {
+  brand: string
+  horsePower: number
+}
+
+// Type aliases
+type Base64 = string
+
+interface Camera {
+  image: Base64
 }
 
 const people: Person[] = [
-  { age: 42, name: 'Sonia', job: 'Autentia' },
-  { age: 32, name: 'Paul' }
+  { age: 42, name: 'Sonia', job: 'Autentia', pet: undefined },
+  { age: 32, name: 'Paul', pet: 'lizard' }
 ]
 const ages = people.reduce((a, b) => a + b.age, 0)
 console.log(ages)
@@ -59,6 +83,7 @@ type GreeterPerson = Person & Greeter
 const greeterPerson: GreeterPerson = {
   age: 54,
   name: 'Sara',
+  pet: undefined,
   greet(a: string): string {
     return 'hello ' + a + ", I'm " + this.name
   }
@@ -93,3 +118,53 @@ class Dog extends Animal {
 
 const dog = new Dog('Tobby')
 dog.move()
+
+class ClassWithPrivateMethodsAndProperties {
+  publicProperty = 42
+  private privateroperty = 42
+
+  publicMethod() {
+    return 42
+  }
+
+  private privateMethod() {
+    return 42
+  }
+}
+
+class ImprovedConstructorAssignment {
+  constructor(public name: string) {}
+
+  move(distance: number) {
+    console.log('Moved ' + distance + ' meters')
+  }
+}
+
+class ReadonlyClass {
+  constructor(
+    public readonly name: string,
+    private readonly foo: number,
+    private readonly person: Person
+  ) {}
+
+  bar(): string {
+    this.person.age = 43
+    return this.name + this.foo
+  }
+}
+
+interface Logger {
+  log(message: string): void
+}
+
+class ConsoleLogger implements Logger {
+  log(message: string) {
+    console.log(message)
+  }
+}
+
+class AlertLogger implements Logger {
+  log(message: string) {
+    alert(message)
+  }
+}
