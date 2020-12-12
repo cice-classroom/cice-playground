125dc66a2ccf93181fc9aa55c2dd98e9028f5fd9
César Alberca <cesalberca@gmail.com>
2020-12-12 12:05:43 +0100

    Add more examples

--- packages/typescript/generics.ts
+++ packages/typescript/generics.ts
@@ -10,12 +10,19 @@ function identity3(arg: boolean): boolean {
   return arg
 }
 
-function identity<T>(arg: T): T {
+function identity<Type>(arg: Type): Type {
   return arg
 }
 
 identity<string>('foo') // string
 identity('foo') // string
+
+function identityWithMoreGenerics<Type, Result>(arg: Type): Type {
+  return arg
+}
+
+identityWithMoreGenerics<string, number>('foo') // string
+identityWithMoreGenerics('foo') // string
 
 function loggingIdentity<T>(arg: T): T {
   console.log(arg.length)
@@ -37,6 +44,16 @@ const generic = new Generic<string>()
 generic.foo = ''
 generic.bar('baz')
 
+class DefaultGeneric<T = string> {
+  foo?: T
+  bar(x: T): T {
+    return x
+  }
+}
+const generic2 = new DefaultGeneric()
+generic2.foo = ''
+generic2.bar('baz')
+
 interface Lengthwise {
   length: number
 }
@@ -44,4 +61,11 @@ interface Lengthwise {
 function loggingConstraintIdentity<T extends Lengthwise>(arg: T): T {
   console.log(arg.length)
   return arg
+}
+
+loggingConstraintIdentity([])
+loggingConstraintIdentity({ length: 2 })
+
+interface Foo<T> {
+  bar(): T
 }
