e974003a9279d5ca937001a73fbc1c426eb8272d
César Alberca <cesalberca@gmail.com>
2021-01-08 17:38:36 +0100

    Create my-solutions and solutions directory

--- packages/10-reactjs/exercises/src/index.tsx
+++ packages/10-reactjs/exercises/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { App } from './app'
+import { App } from './solutions/app'
 import { reportWebVitals } from './report-web-vitals'
 
 ReactDOM.render(
