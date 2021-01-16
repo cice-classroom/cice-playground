f43c94106fe2655df7ad3ef3506ec63d222fd3a8
César Alberca <cesalberca@gmail.com>
2021-01-16 09:40:01 +0100

    Revert

--- packages/10-reactjs/todo-list/src/solutions/features/app.tsx
+++ packages/10-reactjs/todo-list/src/solutions/features/app.tsx
@@ -1,38 +1,22 @@
 import { FC, useState } from 'react'
+import { Todo } from './todo'
 import styles from './app.module.css'
 import { bind } from '../utils/bind'
-import { Button } from './button'
 
 const cx = bind(styles)
 
-interface Todo {
-  id: number
-  text: string
-  completed: boolean
-}
-
 export const App: FC = () => {
-  const [todoText, setTodoText] = useState('')
-  const [todos, setTodos] = useState<Todo[]>([
-    {
-      id: 1,
-      text: 'hola',
-      completed: true
-    },
-    {
-      id: 2,
-      text: 'adios',
-      completed: false
-    }
-  ])
+  const [todos, setTodos] = useState<Todo[]>([])
 
-  function createTodo() {
-    setTodos([
-      ...todos,
-      { id: Math.floor(Math.random() * 10_000), text: todoText, completed: false }
-    ])
-    setTodoText('')
+  function createTodo(todoText: string) {
+    const newTodo: Todo = { id: Math.floor(Math.random() * 1000), text: todoText, completed: false }
+    setTodos([...todos, newTodo])
   }
+
+  const [todoText, setTodoText] = useState('')
+  const isTodoDuplicated = todos.map(todo => todo.text).includes(todoText)
+
+  const clearTodo = () => setTodoText('')
 
   function completeTodo(id: number) {
     setTodos(
@@ -51,27 +35,29 @@ export const App: FC = () => {
 
   return (
     <main>
-      <header>
-        <input type="text" value={todoText} onChange={e => setTodoText(e.target.value)} />
-        <Button
-          onClick={() => {
-            createTodo()
-          }}
-        >
-          <strong>Create todo</strong>
-        </Button>
-      </header>
       <ul>
         {todos.map(todo => (
-          <li
-            onClick={() => completeTodo(todo.id)}
-            key={todo.id}
-            className={cx({ completed: todo.completed })}
-          >
+          <li onClick={() => completeTodo(todo.id)} className={cx({ completed: todo.completed })}>
             {todo.text}
           </li>
         ))}
       </ul>
+      <form
+        onSubmit={event => {
+          event.preventDefault()
+          createTodo(todoText)
+          clearTodo()
+        }}
+      >
+        <label>
+          Todo
+          <input value={todoText} onChange={event => setTodoText(event.target.value)} />
+        </label>
+        <button onClick={clearTodo}>Clear todo</button>
+        <button type="submit" disabled={isTodoDuplicated}>
+          Create todo
+        </button>
+      </form>
     </main>
   )
 }
--- packages/10-reactjs/todo-list/src/solutions/features/button.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-interface Props {
-  onClick(): void
-}
-
-export const Button: FC<Props> = ({ onClick, children }) => (
-  <button onClick={() => onClick()}>{children}</button>
-)
