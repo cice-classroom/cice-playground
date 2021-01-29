8104ab4a02ae085775ceae132ac3a8e3c991a036
César Alberca <cesalberca@gmail.com>
2021-01-29 17:54:55 +0100

    Use CRA for DI

--- packages/10-reactjs/di/src/index.tsx
+++ packages/10-reactjs/di/src/index.tsx
@@ -2,13 +2,16 @@ import ReactDOM from 'react-dom'
 import { container } from './container'
 import { TYPES } from './types'
 import { PersonalAssistant } from './personal-assistant'
+import React from 'react'
 
 const personalAssistant = container.get<PersonalAssistant>(TYPES.PERSONAL_ASSISTANT)
 const personalAssistant2 = container.get<PersonalAssistant>(TYPES.PERSONAL_ASSISTANT)
 
 ReactDOM.render(
-  <h1>
-    {personalAssistant.morningRoutine('César')} - {personalAssistant2.morningRoutine('John')}
-  </h1>,
+  <React.StrictMode>
+    <h1>
+      {personalAssistant.morningRoutine('César')} - {personalAssistant2.morningRoutine('John')}
+    </h1>
+  </React.StrictMode>,
   document.getElementById('root')
 )
